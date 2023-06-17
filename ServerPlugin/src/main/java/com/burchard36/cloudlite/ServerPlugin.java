d07d83544c9b57e75affd7ad5ea06e3f876f1df3
Burchard36 <burchard36@gmail.com>
2023-06-16 22:31:25 -0700

    Remove the music player module as its now standalone

--- ServerPlugin/src/main/java/com/burchard36/cloudlite/ServerPlugin.java
+++ ServerPlugin/src/main/java/com/burchard36/cloudlite/ServerPlugin.java
@@ -7,7 +7,6 @@ public final class ServerPlugin extends CloudLiteCore {
     @Override
     public void onLoad() {
         // Modules need to get injected before the rest of the plugin
-        this.getModuleLoader().registerModule(new CloudLiteMusicPlayer());
         this.getModuleLoader().registerModule(new AutoCompressorModule());
         this.getModuleLoader().registerModule(new MMOItemsLevelUp());
         this.getModuleLoader().registerModule(new BlockDropsEditor());
